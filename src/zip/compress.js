f16d7151d7f25b6dc7c5eacf33f1b366684c33b3
Olga <kartoshkaslukom@gmail.com>
2024-10-04 17:59:19 +0200

    feat: add zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createWriteStream, createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    try{
+        const fileReadStream = createReadStream('src/zip/files/fileToCompress.txt');
+        const fileWriteStream = createWriteStream('src/zip/files/archive.gz');
+        const gzip = createGzip();
+        await pipeline(fileReadStream, gzip, fileWriteStream);
+    } catch (error) {
+        console.error('Error while compression', error);
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { createWriteStream, createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    try{
+        const fileReadStream = createReadStream('src/zip/files/archive.gz');
+        const fileWriteStream = createWriteStream('src/zip/files/fileToCompress.txt');
+        const gunzip = createGunzip();
+        await pipeline(fileReadStream, gunzip, fileWriteStream);
+    } catch (error) {
+        console.error('Error while compression', error);
+    }
 };
 
 await decompress();
