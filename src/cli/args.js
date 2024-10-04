2c9e1423c9197d25a1eca46af9ea67426248e34c
Olga <kartoshkaslukom@gmail.com>
2024-10-04 16:49:40 +0200

    feat: add cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    let result = '';
+    for (let i = 2; i < process.argv.length; i += 2) {
+        let argName = process.argv[i].slice(2);
+        result += `${argName} is ${process.argv[i + 1]}, `;
+    }
+
+    console.log(result.slice(0, -2));
 };
 
 parseArgs();
