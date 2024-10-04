91fdc1ddb33c129285f46e95d1aaf8b39eef33fa
Olga <kartoshkaslukom@gmail.com>
2024-10-04 17:15:01 +0200

    feat: add hashCalculate function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { pipeline } from 'stream/promises';
+import { Writable } from 'stream';
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    let echo = new Writable({
+        write(chunk, encoding, callback) {
+            console.log(chunk.toString());
+            callback();
+        }
+    });
+    await pipeline(
+        createReadStream('src/hash/files/fileToCalculateHashFor.txt'),
+        createHash('sha256').setEncoding('hex'),
+        echo
+    );
 };
 
 await calculateHash();
