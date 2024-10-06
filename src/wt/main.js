d4e3a3e9b3a8ae9f942a77008d93bcc023224ae7
Olga <kartoshkaslukom@gmail.com>
2024-10-06 19:12:17 +0200

    feat: add workers function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { Worker, } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numWorkers = os.cpus().length;
+    const promises = [];
+    const __dirname = import.meta.dirname;
+
+    for (let i = 0; i < numWorkers; i++) {
+        promises.push(
+          new Promise((resolve) => {
+            const worker = new Worker(path.resolve(__dirname, './worker.js'));
+            
+            worker.postMessage(10 + i);
+    
+            worker.on('message', (message) => {
+              resolve(message);
+            });
+    
+            worker.on('error', () => {
+              resolve({ status: 'error', data: null });
+            });
+          })
+        );
+      }
+    
+      Promise.all(promises).then((results) => {
+        console.log(results);
+      });
 };
 
 await performCalculations();
