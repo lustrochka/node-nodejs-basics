14106c282793d919d5f2f9985c993dde8a73c00e
Olga <kartoshkaslukom@gmail.com>
2025-04-27 20:31:14 +0200

    feat: add child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const path = 'src/cp/files/script.js';
+
+    const child = spawn('node', [path, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+      });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [1, 2, 3] );
